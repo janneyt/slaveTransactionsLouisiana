9b6437f264c0bf086be04e048f38932ba8a3ec94
Ted Janney <janneyt@oregonstate.edu>
2021-12-21 23:31:22 +1000

    Setup the serializer to show all fields for the api

--- Slave_Transactions/settings.py
+++ Slave_Transactions/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import django-heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +130,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
